86c8f2f77a65542268d69f85c0537b34aa6b5b02
James Le Cuirot <james.le-cuirot@yakara.com>
2017-06-01 16:44:23 +0100

    Mirror lusis/chef-kibana#129 change here to avoid breakage

--- recipes/kibana.rb
+++ recipes/kibana.rb
@@ -83,6 +83,7 @@ if install_type == 'file'
       home: "#{node['kibana']['install_dir']}/current"
     )
     cookbook 'kibana_lwrp'
+    default_logger true
     subscribes :restart, "template[#{kibana_config}]", :delayed
   end
 
